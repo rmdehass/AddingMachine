7a410ce3210afbc8bad56c06ef3ba0d58084f8f5
rmdehass <rdehass@asu.edu>
2020-10-05 19:16:11 -0700

    Initial version of AddingMachine with comments

---
+++ assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,33 @@
+package cse360assignment02;
+/**
+ * 
+ * @author Ryan
+ *
+ */
+public class AddingMachine {
+  private int total;
+  /**
+   * AddingMachine class
+   */
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+  }
+
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
